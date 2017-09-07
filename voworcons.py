3f1dbdecb48ec895202c332e420c03b1949a618b
karthik59 <30342623+karthik59@users.noreply.github.com>
2017-09-07 14:05:01 +0530

    Create voworcons.py

---
+++ voworcons.py
@@ -0,0 +1,6 @@
+def main():
+karthik = char(input("enter the character"))
+if (karthik == a or e or i or o or u):
+  print ("karthik is vowel",karthik)
+else:
+  print("karthik is consonent",karthik)
