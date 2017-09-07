736144caaf005fd9801383cdb60d2b5f1a476be0
karthik59 <30342623+karthik59@users.noreply.github.com>
2017-09-07 13:23:39 +0530

    Create variableposnegzero.py

---
+++ variableposnegzero.py
@@ -0,0 +1,8 @@
+def main():
+a=int(input("enter the number a"))
+if (a>0):
+  print ("number a is positive",a)
+elif(a<0):
+  print ("number a is negative",a)
+elif(a==0):
+  print ("number a is zero",a)
